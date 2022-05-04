d7f911db59250cf63c75f17a863d8e3932d5ef91
Isuru Rajitha <49783984+IsuruRajithaGamage@users.noreply.github.com>
2022-05-04 22:52:48 +0530

    Add files via upload

---
+++ indexD3_Additional.js
@@ -0,0 +1,23 @@
+const nums1 = [1, 2, 2, 1, 8];
+const nums2 = [2, 2, 8];
+var common = [];
+var icurrent = [];
+var jcurrent = [];
+
+Array.prototype.intersection = function (array) {
+  for (let i = 0; i < nums1.length; i++) {
+    icurrent.push(nums1[i]);
+    for (let j = 0; j < nums2.length; j++) {
+      jcurrent.push(nums2[j]);
+      if (nums1[i] === nums2[j] && icurrent[i - 1] != jcurrent[j]) {
+        common.push(nums1[i]);
+
+        // console.log(nums1[i]);
+      }
+    }
+  }
+};
+
+// intersection(nums1, nums2);
+nums1.intersection(nums2);
+console.log(common);
